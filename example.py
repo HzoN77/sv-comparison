119751b6bd25ab190a2c2d05c7f14a028caded05
Jens Henriksson <Jens.Henriksson@semcon.com>
2019-02-08 10:47:58 +0100

    Refactor: Added better variable names for number of samples and labels

--- example.py
+++ example.py
@@ -3,16 +3,17 @@ import numpy as np
 
 if __name__=="__main__":
     # Make an example anomaly score distribution, and split between them.
-    n = 1000
-    l = 10
+    numSamples = 1000
+    numLabels = 10
+    outlierLabel = 123
 
-    aScoreIn = np.random.normal(1, 1, n)
-    labelsIn = np.random.randint(0, l, n)
-    aScoreOOD = np.random.normal(6, 1, n)
+    aScoreIn = np.random.normal(1, 1, numSamples)
+    labelsIn = np.random.randint(0, numLabels, numSamples)
+    aScoreOOD = np.random.normal(6, 1, numSamples)
 
     lt = labelsIn  # Assume 100% Accuracy for this case
-    labelsOOD = np.random.randint(0, l, n)
-    ltOOD = 10 * np.ones(n)
+    labelsOOD = np.random.randint(0, numLabels, numSamples)
+    ltOOD = outlierLabel * np.ones(numSamples)
 
     anomalyScores = np.concatenate([aScoreIn, aScoreOOD])
     labels = np.concatenate([labelsIn, labelsOOD])
