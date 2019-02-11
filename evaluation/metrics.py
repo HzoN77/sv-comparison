dd3e78eb30914cd63fbeddfaf3628c6b86c2fc84
Jens Henriksson <Jens.Henriksson@semcon.com>
2019-02-11 15:34:33 +0100

    Refactor: Moved multiple uses of code to separate functions. Also, added the result to class variables for later re-use.

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -8,11 +8,34 @@ class MetricPlots(object):
     Returns a ```Metric&Plots``` object with the option to plot.
 
     """
-    def __init__(self, anomalyScoreData, predictionLabels, trueLabels, outlierLabel):
+    def __init__(self, anomalyScoreData, predictionLabels, trueLabels, outlierLabel, bins=500):
             self.anomalyScoreData = anomalyScoreData
             self.predictionLabels = predictionLabels
             self.trueLabels = trueLabels
             self.outlierLabel = outlierLabel
+            self.bins = bins
+            self.tpr, self.fpr, self.precision = self.calculate_roc_metrics()
+
+    def calculate_roc_values(self, bins=None):
+        if not bins:
+            bins = self.bins
+
+        fpr = np.zeros(bins)
+        tpr = np.zeros(bins)  # also known as recall
+        precision = np.ones(bins)
+        for i, th in zip(range(bins), np.linspace(self.anomalyScoreData.max(), self.anomalyScoreData.min(), bins)):
+            tp = float(self.anomalyScoreData[(self.trueLabels == self.outlierLabel) & (self.anomalyScoreData > th)].shape[0])
+            tn = float(self.anomalyScoreData[(self.trueLabels != self.outlierLabel) & (self.anomalyScoreData < th)].shape[0])
+            fp = float(self.anomalyScoreData[(self.trueLabels != self.outlierLabel) & (self.anomalyScoreData > th)].shape[0])
+            fn = float(self.anomalyScoreData[(self.trueLabels == self.outlierLabel) & (self.anomalyScoreData < th)].shape[0])
+
+            if fp > 0:
+                fpr[i] = fp / (tn + fp)
+            if tp > 0:
+                tpr[i] = tp / (tp + fn)
+                precision[i] = tp / (tp + fp)
+
+        return tpr, fpr, precision
 
     def plot_distributions(self, bins=30):
         fig, ax1 = plt.subplots(figsize=(7, 4))
@@ -28,25 +51,14 @@ class MetricPlots(object):
         plt.draw()
 
     def plot_ROC_curve(self, bins=500):
-        fpr = np.zeros(bins)
-        tpr = np.zeros(bins)
+        if bins != self.bins:
+            self.calculate_roc_values(bins=bins)  # Update fpr, tpr and precision if different bin size.
 
-        for i, th in zip(range(bins), np.linspace(self.anomalyScoreData.max(), self.anomalyScoreData.min(), bins)):
-            tp = float(self.anomalyScoreData[(self.trueLabels == self.outlierLabel) & (self.anomalyScoreData > th)].shape[0])
-            tn = float(self.anomalyScoreData[(self.trueLabels != self.outlierLabel) & (self.anomalyScoreData < th)].shape[0])
-            fp = float(self.anomalyScoreData[(self.trueLabels != self.outlierLabel) & (self.anomalyScoreData > th)].shape[0])
-            fn = float(self.anomalyScoreData[(self.trueLabels == self.outlierLabel) & (self.anomalyScoreData < th)].shape[0])
-
-            if fp > 0:
-                fpr[i] = fp / (tn + fp)
-            if tp > 0:
-                tpr[i] = tp / (tp + fn)
-
-        auroc = np.trapz(tpr, fpr)
+        auroc = np.trapz(self.tpr, self.fpr)
 
         # plot ROC
         fig, ax1 = plt.subplots(figsize=(7, 4))
-        plt.plot(fpr, tpr, 'b-')
+        plt.plot(self.fpr, self.tpr, 'b-')
         plt.title('area under curve = ' + str(round(auroc, 3)))
         plt.xlabel('FPR')
         plt.ylabel('TPR')
@@ -54,26 +66,14 @@ class MetricPlots(object):
         plt.draw()
 
     def plot_precision_recall_curve(self, bins=500):
+        if bins != self.bins:
+            self.calculate_roc_values(bins=bins)  # Update fpr, tpr and precision.
 
-        precision = np.ones(bins)
-        recall = np.zeros(bins)  # recall is another name for true positive rate.
-
-        for i, th in zip(range(bins), np.linspace(self.anomalyScoreData.max(), self.anomalyScoreData.min(), bins)):
-            tp = float(self.anomalyScoreData[(self.trueLabels == self.outlierLabel) & (self.anomalyScoreData > th)].shape[0])
-            tn = float(self.anomalyScoreData[(self.trueLabels != self.outlierLabel) & (self.anomalyScoreData < th)].shape[0])
-            fp = float(self.anomalyScoreData[(self.trueLabels != self.outlierLabel) & (self.anomalyScoreData > th)].shape[0])
-            fn = float(self.anomalyScoreData[(self.trueLabels == self.outlierLabel) & (self.anomalyScoreData < th)].shape[0])
-
-            if tp > 0:
-                precision[i] = tp/(tp + fp)
-            if tp > 0:
-                recall[i] = tp / (tp + fn)
-
-        aupr = np.trapz(precision, recall)
+        aupr = np.trapz(self.precision, self.tpr)
 
         # Plot precision-recall curve
         fig, ax1 = plt.subplots(figsize=(7, 4))
-        plt.plot(recall, precision, 'b-')
+        plt.plot(self.tpr, self.precision, 'b-')
         plt.title('area under PR-curve= ' + str(round(aupr, 3)))
         plt.xlabel('recall')
         plt.ylabel('precision')
