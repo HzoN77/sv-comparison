d11e76e4e42152c917d162377c97b83e39a782ad
Jens Henriksson <Jens.Henriksson@semcon.com>
2019-02-22 14:02:52 +0100

    Added dataset loader for MNIST and CIFAR10

---
+++ datasets/loader.py
@@ -0,0 +1,13 @@
+from torchvision.datasets import MNIST, CIFAR10
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+
+def load_mnist(transform=ToTensor(), train=True, batch_size=32, shuffle=True, num_workers=2):
+    mnist = MNIST('./datasets/mnist/', train=train, transform=transform, download=True)
+    return DataLoader(mnist, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+def load_cifar10(transform=ToTensor(), train=True, batch_size=32, shuffle=True, num_workers=2):
+    mnist = CIFAR10('./datasets/cifar10/', train=train, transform=transform, download=True)
+    return DataLoader(mnist, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+
