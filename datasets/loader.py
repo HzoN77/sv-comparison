4b4283a8d10477b010cc78c0601bfd24265f5b10
Jens Henriksson <Jens.Henriksson@semcon.com>
2019-02-22 14:04:23 +0100

    Added loader for Tiny ImageNet.
    Added dummy-loader for Omniglot.

--- datasets/loader.py
+++ datasets/loader.py
@@ -10,4 +10,11 @@ def load_cifar10(transform=ToTensor(), train=True, batch_size=32, shuffle=True, 
     mnist = CIFAR10('./datasets/cifar10/', train=train, transform=transform, download=True)
     return DataLoader(mnist, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
+def load_tiny_imagenet(transform=ToTensor(), train=True, batch_size=32, shuffle=True, num_workers=2):
+    from datasets.tinyimagenet import TinyImageNet
+    tiny = TinyImageNet('./datasets/tiny-imagenet-200/val/images/', transform=transform)
+    return DataLoader(tiny, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+def load_omniglot(transform=ToTensor(), train=True, batch_size=32, shuffle=True, num_workers=2):
+    pass
 
