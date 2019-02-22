28628a681b993c878861de4a4cdf6711e5b6aab6
Jens Henriksson <Jens.Henriksson@semcon.com>
2019-02-22 15:20:23 +0100

    Added an example network suitable for CIFAR10.

--- example_cifar_train.py
+++ example_cifar_train.py
@@ -7,12 +7,36 @@ import torchvision.transforms as transforms
 from datasets.loader import load_cifar10
 import matplotlib.pyplot as plt
 import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
 
 def imshow(img):
     img = img / 2 + 0.5  # Unnormalize from transform.
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 
 if __name__=="__main__":
     transform = transforms.Compose([
@@ -27,3 +51,6 @@ if __name__=="__main__":
     images, labels = dataiter.next()
 
     imshow(torchvision.utils.make_grid(images))
+
+    # Define a network
+    net = Net()
