edbe05d7d4bb4e2d37db7b8cda8ff108b15e72ac
Jens Henriksson <Jens.Henriksson@semcon.com>
2019-02-25 15:02:09 +0100

    Refactor: Moved CIFAR simple neural net to separate class, so it can be used with saved weights.

--- example_cifar_train.py
+++ example_cifar_train.py
@@ -2,14 +2,14 @@
 # https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
 import torch
-import torchvision
 import torchvision.transforms as transforms
 from datasets.loader import load_cifar10
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
 import os
+
+from models.cifar_example_net import Net
 from utils.pytorch_eval import pytorch_train
 
 def imshow(img):
@@ -17,26 +17,6 @@ def imshow(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 
 if __name__=="__main__":
@@ -51,11 +31,15 @@ if __name__=="__main__":
     dataiter = iter(trainloader)
     images, labels = dataiter.next()
 
-    imshow(torchvision.utils.make_grid(images))
+    # imshow(torchvision.utils.make_grid(images))
 
     # Define a network
     net = Net()
 
+    # from models.vgg import VGG
+    # net = VGG('VGG16')
+
+    print("Network created")
     # Define a loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
