2156767102f720a9f04623c8c72ec362cfca04d0
Jens Henriksson <Jens.Henriksson@semcon.com>
2019-02-26 12:58:00 +0100

    Added pytorch test (to give accuracy) and pytorch predice to return predictions

--- utils/pytorch_eval.py
+++ utils/pytorch_eval.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn.functional as F
 
 def pytorch_train(model, dataloader, device, optimizer, criterion, epoch=0):
+    model.train()
     running_loss = 0.0
     for i, data in enumerate(dataloader, 0):
         # get the inputs
@@ -22,3 +24,43 @@ def pytorch_train(model, dataloader, device, optimizer, criterion, epoch=0):
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
+
+def pytorch_test(model, device, dataloader):
+    model.eval()
+    test_loss = 0.0
+    correct = 0
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+            outputs = model(images.to(device))
+
+            _, predicted = torch.max(outputs.data, 1)
+            test_loss += F.nll_loss(outputs, labels.to(device))  # sum up batch loss
+            correct += predicted.eq(labels.to(device)).sum().item()
+
+    test_loss /= len(dataloader.dataset)
+    accuracy = 100. * correct / len(dataloader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(dataloader.dataset), accuracy))
+
+    return test_loss, accuracy
+
+
+def pytorch_predict(model, device, dataloader, return_labels=True):
+    model.eval()
+    preds = []
+    targets = []
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+            outputs = model(images.to(device))
+            for out in outputs:
+                preds.append(out)
+            if return_labels == True:
+                for targ in labels.to(device):
+                    targets.append(targ)
+
+    if return_labels == True:
+        return preds, targets
+    else:
+        return preds
